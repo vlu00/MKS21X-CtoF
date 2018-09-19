83bfbb90fd202957b9013718489b6321c1f27c43
vlu00 <vlu00@stuy.edu>
2018-09-19 17:45:59 -0400

    Both functions work

--- CtoFTester.java
+++ CtoFTester.java
@@ -3,7 +3,7 @@ public class CtoFTester {
     return C * 1.8 + 32;
   }
   public static double fahrenheitToCelsius (double F) {
-    return (F - 32) * 1.8;
+    return (F - 32) / 1.8;
   }
   public static void main (String [] args) {
     System.out.println (celsiusToFahrenheit (0) );
