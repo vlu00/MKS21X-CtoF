94e39cb7e3b0f30adbbe5defa39e8c159391031f
vlu00 <vlu00@stuy.edu>
2018-09-19 17:37:48 -0400

    Created fahrenheitToCelsius but there's something wrong with the conversion

--- CtoFTester.java
+++ CtoFTester.java
@@ -2,9 +2,15 @@ public class CtoFTester {
   public static double celsiusToFahrenheit (double C) {
     return C * 1.8 + 32;
   }
+  public static double fahrenheitToCelsius (double F) {
+    return (F - 32) * 1.8;
+  }
   public static void main (String [] args) {
     System.out.println (celsiusToFahrenheit (0) );
     System.out.println (celsiusToFahrenheit (100) );
     System.out.println (celsiusToFahrenheit (52) );
+    System.out.println (fahrenheitToCelsius (0) );
+    System.out.println (fahrenheitToCelsius (100) );
+    System.out.println (fahrenheitToCelsius (52) );
   }
 }
